8f0bddc275609d046ddc2c95136f96eb9b6987a3
Chikwendu Chibudom <chibudomchikwendu@gmail.com>
2023-08-31 19:05:21 +0100

    removed excesses in app.js

--- src/App.js
+++ src/App.js
@@ -24,36 +24,6 @@ function App() {
             <br />The firm, owned by South Asian giant Sea, said it has partnered with Yotta, controlled by local giant Hiranandani, for cloud and other storage needs of local users’ data in the country. It has also appointed Indian cricket legend Mahendra Singh Dhoni as its brand ambassador in the country.</p>
         </div>
       </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1237931798.jpg?w=990&crop=1" alt="Content"></img>
-        </div>
-        <div className="texts">
-          <h2>Free Fire in India a year after ban</h2><br />
-          <p className="info">
-            <a className="author">Uzo Sebastine</a>
-            <time>2023-08-31 18:00</time>
-          </p><br />
-          <p className="summary">
-            Garena is relaunching Free Fire in India, a year and a half after the popular mobile title was banned in the South Asian market over national security concerns.<br />
-            <br />The firm, owned by South Asian giant Sea, said it has partnered with Yotta, controlled by local giant Hiranandani, for cloud and other storage needs of local users’ data in the country. It has also appointed Indian cricket legend Mahendra Singh Dhoni as its brand ambassador in the country.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1237931798.jpg?w=990&crop=1" alt="Content"></img>
-        </div>
-        <div className="texts">
-          <h2>Free Fire in India a year after ban</h2><br />
-          <p className="info">
-            <a className="author">Uzo Sebastine</a>
-            <time>2023-08-31 18:00</time>
-          </p><br />
-          <p className="summary">
-            Garena is relaunching Free Fire in India, a year and a half after the popular mobile title was banned in the South Asian market over national security concerns.<br />
-            <br />The firm, owned by South Asian giant Sea, said it has partnered with Yotta, controlled by local giant Hiranandani, for cloud and other storage needs of local users’ data in the country. It has also appointed Indian cricket legend Mahendra Singh Dhoni as its brand ambassador in the country.</p>
-        </div>
-      </div>
     </main>
   );
 }
