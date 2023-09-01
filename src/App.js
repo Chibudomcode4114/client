1b7e1c4e067e5c674d1dd418accb146584b82022
Chikwendu Chibudom <chibudomchikwendu@gmail.com>
2023-09-01 19:16:50 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import './App.css';
+import Post from './Post';
+import Header from './Header'
+import { Route, Routes } from "react-router-dom"
+
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1237931798.jpg?w=990&crop=1" alt="Content"></img>
-        </div>
-        <div className="texts">
-          <h2>Free Fire in India a year after ban</h2><br />
-          <p className="info">
-            <a className="author">Uzo Sebastine</a>            <time>2023-08-31 18:00</time>
-          </p><br />
-          <p className="summary">
-            Garena is relaunching Free Fire in India, a year and a half after the popular mobile title was banned in the South Asian market over national security concerns.<br />
-            <br />The firm, owned by South Asian giant Sea, said it has partnered with Yotta, controlled by local giant Hiranandani, for cloud and other storage needs of local users’ data in the country. It has also appointed Indian cricket legend Mahendra Singh Dhoni as its brand ambassador in the country.</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+      <Route path={'/login'} elements={
+        <main>
+          <Header />
+          <div>login page</div>
+        </main>
+      } />
+    </Routes>
   );
 }
 
